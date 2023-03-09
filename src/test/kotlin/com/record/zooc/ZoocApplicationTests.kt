52356320da43d26b0901b8467fc1532a388bb4ba
jinsu4755 <parkjinsu4755@gmail.com>
2023-03-09 21:36:14 +0900

    [feat] init project

---
+++ src/test/kotlin/com/record/zooc/ZoocApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.record.zooc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZoocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
